5fd0f0ae3ca9c737f3e6c85710460ffa079f4f3a
Pailleux Jean-didier <Pailleux Jean-didier>
2017-05-08 14:24:56 +0200

    Modif template

---
+++ app/main/interface_web/static/uploads/runserver.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import sys
+import argparse
+import webbrowser
+from interface_web.gestionFlux import app
+
+parser = argparse.ArgumentParser(description="Uploadr")
+parser.add_argument(
+    "--port", "-p",
+    type=int,
+    help="Port to listen on",
+    default=2006,
+)
+args = parser.parse_args()
+
+# Fonction main
+if __name__ == '__main__':
+    flask_options = dict(
+        debug=False,
+        port=args.port,
+        threaded=True,
+    )
+    webbrowser.open_new('http://127.0.0.1:2006/')
+    app.run(**flask_options)
+
