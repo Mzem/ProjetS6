07fff92fb4cf43a39bcc5f17bc56ec7efcba7e5d
Pailleux Jean-didier <Pailleux Jean-didier>
2017-05-28 13:15:41 +0200

    .bat

--- app/make.bat
+++ app/make.bat
@@ -1,17 +1,57 @@
-cls
+
 @echo off
 
-setlocal
-set PATH=%PATH%;C:\mingw\bin;C:\mingw\msys\1.0\bin
+REM Règle pour le lancement de l'application
+:run
+cd main\
+start python runserver.py
+goto end
 
-echo *************************************************
-echo This script compile you Windows  project
-echo *************************************************
-echo PREREQUISITE:
-echo Have MinGW installed on your computer
+REM Règle pour supprimer les .pyc et lancer les tests
+:all
+test
+goto end
 
-call make run
+REM Règles pour le lancement des tests
+:test-interface_web
+cd test\test_interface_web
+start python -m unittest discover
+goto end
 
-echo Now your driver is ready
+:test-add
+cd test\test_add
+start python -m unittest discover
+goto end
 
-pause
+:test-chargement_des_donnees
+cd test\test_chargement_des_donnees
+start python -m unittest discover
+goto end
+
+:test
+cd test\test_interface_web
+start python -m unittest discover
+cd ..\test_add
+start python -m unittest discover
+cd..\test_chargement_des_donnees
+start python -m unittest discover
+cd ..
+goto end
+
+REM Règle pour générer et afficher la documentation au format html
+:doc-html
+cd doc/
+start make html
+google-chrome doc/build/html/index.html
+goto end
+
+REM Règle pour générer la documentation au format latex
+:doc-latexpdf
+cd doc/
+start make latexpdf
+xdg-open doc/build/latex/FilRouge.pdf &
+goto end
+
+:end
+cd ..
+popd
